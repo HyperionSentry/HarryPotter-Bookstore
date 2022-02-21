41c5c5c27bab757cebc482e526bb2c63f3cea1c5
Fernando Javier Caballero <fer.caballero21@gmail.com>
2022-02-20 22:33:24 -0300

    Agregado de casi toda la logica

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+/*
+Usar objetos para dibujar los items en las cartas
+
+*/
+
+
+const addToShoppingCartButtons = document.querySelectorAll(".addToCart");
+
+addToShoppingCartButtons.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', (e) => {
+        const button = e.target;
+        const item = button.closest('.item');
+        
+        const itemTitle = item.querySelector('.item-title').textContent;
+        const itemPrice = item.querySelector('.item-price').textContent;
+        const itemImage = item.querySelector('.item-image').src;
+
+        addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+
+    });
+           
+});
+
+const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage){
+
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName('shoppingCartItemTitle')
+    for (let i = 0; i< elementsTitle.length ;i++){
+        if(elementsTitle[i].innerText === itemTitle){
+            console.log(itemTitle);
+        }
+    }
+
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `<div class="row shoppingCartItem">
+    <div class="col-6">
+        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src=${itemImage} class="shopping-cart-image" style>
+            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
+            </h6>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+        </div>
+    </div>
+    <div class="col-4">
+        <div
+            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+            <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                value="1">
+            <button class="btn btn-danger buttonDelete" type="button">X</button>
+        </div>
+    </div>
+    </div>`
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow.querySelector('.buttonDelete').addEventListener('click', removeShoppingCartItem);
+    shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change',quantityChanged);
+
+    updateShoppingCartTotal();
+    
+}
+
+function updateShoppingCartTotal(){
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+    const shoppingCartItems = document.querySelectorAll ('.shoppingCartItem');
+
+    shoppingCartItems.forEach((shoppingCartItem) => {
+        const shoppingCartPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+        const shoppingCartItemPrice = Number(shoppingCartPriceElement.textContent.replace('$',''));
+        
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+        
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+        
+    })
+    shoppingCartTotal.innerHTML = `${total}$`;
+}
+
+function removeShoppingCartItem(e){
+    const buttonClicked = e.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+}
+
+function quantityChanged(e){
+    const input = e.target;
+    input.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCartTotal();
+}
