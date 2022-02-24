39edb2641d498b5319adfbf45104af3cb90e8e50
Fernando Javier Caballero <fer.caballero21@gmail.com>
2022-02-24 18:05:49 -0300

    Evitar que se duplique la seleccion, incrmento de contador cuando es duplicado y mensaje de compra ok y limpieza de elementos

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,9 @@ addToShoppingCartButtons.forEach(addToCartButton => {
            
 });
 
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+
 const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
 
 function addItemToShoppingCart(itemTitle, itemPrice, itemImage){
@@ -28,32 +31,36 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage){
     const elementsTitle = shoppingCartItemsContainer.getElementsByClassName('shoppingCartItemTitle')
     for (let i = 0; i< elementsTitle.length ;i++){
         if(elementsTitle[i].innerText === itemTitle){
-            console.log(itemTitle);
+            let elementQuantity = elementsTitle[i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity');
+            elementQuantity.value++;
+            updateShoppingCartTotal();
+            return;
         }
     }
 
     const shoppingCartRow = document.createElement('div');
-    const shoppingCartContent = `<div class="row shoppingCartItem">
-    <div class="col-6">
-        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <img src=${itemImage} class="shopping-cart-image" style>
-            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
-            </h6>
+    const shoppingCartContent = 
+    `<div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImage} class="shopping-cart-image" style>
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
+                </h6>
+            </div>
         </div>
-    </div>
-    <div class="col-2">
-        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
         </div>
-    </div>
-    <div class="col-4">
-        <div
-            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-            <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                value="1">
-            <button class="btn btn-danger buttonDelete" type="button">X</button>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
         </div>
-    </div>
     </div>`
     shoppingCartRow.innerHTML = shoppingCartContent;
     shoppingCartItemsContainer.append(shoppingCartRow);
@@ -92,5 +99,10 @@ function removeShoppingCartItem(e){
 function quantityChanged(e){
     const input = e.target;
     input.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCartTotal();
+}
+
+function comprarButtonClicked (){
+    shoppingCartItemsContainer.innerHTML = '';
     updateShoppingCartTotal();
 }
